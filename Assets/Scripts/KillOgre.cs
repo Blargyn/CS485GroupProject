c9d722d1c7bee481aba784ab063a852287d5b094
jjwlkr <43529044+jjwlkr@users.noreply.github.com>
2018-10-28 11:10:00 -0700

    added jumping, made basic enemies that can be killed when jumped on, and made it so character can't run out of the camera view

---
+++ Assets/Scripts/KillOgre.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOgre : MonoBehaviour
+{
+    public GameObject target;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {        
+        if(collision.transform.GetComponent<PlayerController>() != null)
+        {
+            Destroy(target.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public LayerMask ground;
+    public Transform groundcheck;
+    float velX;
+    float velY;
+    bool facingRight = true;
+    Rigidbody2D rigBody;
+    Animator anim;
+    bool isRunning =  false;
+    bool canJump = false;
+    float minX;
+    float maxX;
+	// Use this for initialization
+	void Start () {
+        rigBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        minX = -bounds.x + .5f;
+        maxX = bounds.x - .5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rigBody.velocity.y;
+        rigBody.velocity = new Vector2(velX * moveSpeed, velY);
+        canJump = Physics2D.OverlapCircle(groundcheck.position, 0.15f, ground);
+
+        if (velX != 0)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+        anim.SetBool("isRunning",isRunning);
+
+        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            canJump = false;
+            rigBody.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+
+        if (transform.position.x < minX)
+        {
+            Vector3 temp = transform.position;
+            temp.x = minX;
+            transform.position = temp;
+        }
+
+        if (transform.position.x > maxX)
+        {
+            Vector3 temp = transform.position;
+            temp.x = maxX;
+            transform.position = temp;
+        }
+    }
+
+    void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+        if (velX > 0)
+        {
+            facingRight = true;
+        }
+        else if (velX < 0)
+        {
+            facingRight = false;
+                }
+        if(((facingRight) &&(localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+        transform.localScale = localScale;
+    }
+}
